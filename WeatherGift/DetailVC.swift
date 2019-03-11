a4cdeac72b80fc63b392040717d6e06872fa5f26
James Steele <jwsteele1@me.com>
2019-03-10 20:02:02 -0400

    initial commit. 7.2 complete

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by James Steele on 3/10/19.
+//  Copyright © 2019 James Steele. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,19 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by James Steele on 3/10/19.
+//  Copyright © 2019 James Steele. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
